f6fdd7bae4147f6bee3e4e7e1d79b9e7e20f39bf
Juan Manuel Gentili <gentilijuanmanuel80974a@gmail.com>
2018-01-20 10:43:20 -0300

    Implementation of testimonies

--- Project/Model/Testimonio.cs
+++ Project/Model/Testimonio.cs
@@ -5,6 +5,8 @@ namespace Model
     using System.ComponentModel.DataAnnotations;
     using System.ComponentModel.DataAnnotations.Schema;
     using System.Data.Entity.Spatial;
+    using System.Linq;
+    using System.Data.Entity;
 
     [Table("Testimonio")]
     public partial class Testimonio
@@ -30,5 +32,94 @@ namespace Model
         public string Fecha { get; set; }
 
         public virtual Usuario Usuario { get; set; }
+
+        //Methods
+
+        public List<Testimonio> getAll(int usuario_id)
+        {
+            List<Testimonio> testimonios = new List<Testimonio>();
+
+            using (var ctx = new ProjectContext())
+            {
+                testimonios = ctx.Testimonio.Where(x => x.Usuario_id == usuario_id)
+                                              .ToList();
+            }
+
+            return testimonios;
+        }
+
+        public Testimonio getTestimony(int id)
+        {
+            var testimonio = new Testimonio();
+
+            try
+            {
+                using (var ctx = new ProjectContext())
+                {
+                    testimonio = ctx.Testimonio.Where(x => x.id == id)
+                                               .SingleOrDefault();
+
+                }
+            }
+            catch (Exception E)
+            {
+
+                throw;
+            }
+
+            return testimonio;
+        }
+
+        public ResponseModel Save()
+        {
+            var rm = new ResponseModel();
+
+            try
+            {
+                using (var ctx = new ProjectContext())
+                {
+                    if (this.id > 0)
+                    {
+                        ctx.Entry(this).State = EntityState.Modified;
+                    }
+                    else
+                    {
+                        ctx.Entry(this).State = EntityState.Added;
+                    }
+
+                    ctx.SaveChanges();
+
+                    rm.SetResponse(true);
+                }
+            }
+            catch (Exception E)
+            {
+
+                throw;
+            }
+
+            return rm;
+        }
+
+
+        public void Delete(int id)
+        {
+            try
+            {
+                using (var ctx = new ProjectContext())
+                {
+                    this.id = id;
+
+                    ctx.Entry(this).State = EntityState.Deleted;
+
+                    ctx.SaveChanges();
+                }
+            }
+            catch (Exception E)
+            {
+
+                throw;
+            }
+        }
     }
 }
--- Project/Project/Areas/Admin/Controllers/TestimoniosController.cs
+++ Project/Project/Areas/Admin/Controllers/TestimoniosController.cs
@@ -4,16 +4,54 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Model;
+using Helper;
 
 namespace Project.Areas.Admin.Controllers
 {
+    [Autenticado]
     public class TestimoniosController : Controller
     {
-        [Autenticado]
-        // GET: Admin/Testimonios
+        
+        private Testimonio testimonio = new Testimonio();
+
+        // GET: Admin/Testimonio
         public ActionResult Index()
         {
-            return View();
+            var usuario_id = SessionHelper.GetUser();
+            return View(testimonio.getAll(usuario_id));
+        }
+
+        public ActionResult Crud(int id = 0)
+        {
+            testimonio = testimonio.getTestimony(id);
+
+            return View(testimonio);
+        }
+
+        public JsonResult Save(Testimonio model)
+        {
+            var rm = new ResponseModel();
+
+            if (ModelState.IsValid)
+            {
+                rm = model.Save();
+
+                if (rm.response)
+                {
+                    rm.href = Url.Content("~/admin/testimonios");
+                }
+            }
+
+            return Json(rm);
+        }
+
+
+        public ActionResult Delete(int id = 0)
+        {
+            testimonio.Delete(id);
+
+            return Redirect("~/admin/testimonios");
         }
     }
 }
