f28e18ed1cbaf353538bb9216d0e2f155bdb88cb
Juan Manuel Gentili <gentilijuanmanuel80974a@gmail.com>
2018-01-23 00:27:12 -0300

    Implementation of contact

--- Project/Project/Controllers/DefaultController.cs
+++ Project/Project/Controllers/DefaultController.cs
@@ -5,6 +5,8 @@ using System.Web;
 using System.Web.Mvc;
 using Model;
 using Project.App_Start;
+using Project.ViewModels;
+using System.Net.Mail;
 
 namespace Project.Controllers
 {
@@ -15,6 +17,51 @@ namespace Project.Controllers
         public ActionResult Index()
         {
             return View(usuario.getUser(FrontOfficeStartApp.UsuarioVisualizado(), true));
+        }
+
+        //para pulirlo un poco, pero anda.
+        public ActionResult EnviarCorreo(ContactoViewModel model)
+        {
+            var rm = new ResponseModel();
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    var _usuario = usuario.getUser(FrontOfficeStartApp.UsuarioVisualizado(), false);
+
+                    var mail = new MailMessage();
+                    mail.From = new MailAddress(model.Correo, model.Nombre);
+                    mail.To.Add(_usuario.Email);
+                    mail.Subject = "Correo desde contacto";
+                    mail.IsBodyHtml = true;
+                    mail.Body = model.Mensaje;
+
+                    var smtpServer = new SmtpClient("smtp.gmail.com");
+                    smtpServer.Port = 587;
+                    smtpServer.DeliveryMethod = SmtpDeliveryMethod.Network;
+                    smtpServer.UseDefaultCredentials = false;
+                    smtpServer.Credentials = new System.Net.NetworkCredential("gentilijuanmanuel80974a@gmail.com", "contraseña");
+                    smtpServer.EnableSsl = true;
+                    smtpServer.Send(mail);
+                }
+                catch (Exception e)
+                {
+                    rm.SetResponse(false, e.Message);
+                    return Json(rm);
+                    throw;
+                }
+
+                rm.SetResponse(true);
+                if (rm.response)
+                {
+                    ViewBag.Title = "Index";
+                    return Redirect("~/");
+                }
+                //rm.function = "CerrarContacto();";
+            }
+
+            return Json(rm);
         }
     }
 }
---
+++ Project/Project/ViewModels/ContactoViewModel.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace Project.ViewModels
+{
+    public class ContactoViewModel
+    {
+        private string nombre;
+
+        [Required]
+        public string Nombre
+        {
+            get { return nombre; }
+            set { nombre = value; }
+        }
+
+        private string correo;
+
+        [Required]
+        [EmailAddress]
+        public string Correo
+        {
+            get { return correo; }
+            set { correo = value; }
+        }
+
+        private string mensaje;
+
+        [Required]
+        public string Mensaje
+        {
+            get { return mensaje; }
+            set { mensaje = value; }
+        }
+
+
+
+    }
+}
