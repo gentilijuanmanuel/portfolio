52b3efea4b05e8e7e1f745a6636cc5e59d1ca942
Juan Manuel Gentili <gentilijuanmanuel80974a@gmail.com>
2018-01-25 12:24:45 -0300

    Custom error page

---
+++ Project/Project/Controllers/ErrorController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Project.Controllers
+{
+    public class ErrorController : Controller
+    {
+        // GET: Error
+        public ActionResult Index(int error = 0)
+        {
+            switch (error)
+            {
+                case 505:
+                    ViewBag.Title = "Ocurrio un error inesperado";
+                    ViewBag.Description = "Esperemos que no vuelva a pasar...";
+                    break;
+
+                case 404:
+                    ViewBag.Title = "Página no encontrada";
+                    ViewBag.Description = "La URL a la que está intentando ingresar no existe";
+                    break;
+
+                default:
+                    ViewBag.Title = "Página no encontrada";
+                    ViewBag.Description = "Algo salió mal :(";
+                    break;
+            }
+
+            return View("~/views/error/_ErrorPage.cshtml");
+        }
+    }
+}
--- Project/Project/Global.asax.cs
+++ Project/Project/Global.asax.cs
@@ -14,5 +14,18 @@ namespace Project
             AreaRegistration.RegisterAllAreas();
             RouteConfig.RegisterRoutes(RouteTable.Routes);
         }
+
+        protected void Application_Error(object sender, EventArgs e)
+        {
+            Exception exception = Server.GetLastError();
+            Response.Clear();
+
+            HttpException httpException = exception as HttpException;
+
+            int error = httpException != null ? httpException.GetHttpCode() : 0;
+
+            Server.ClearError();
+            Response.Redirect(String.Format("~/Error/?error={0}", error, exception.Message));
+        }
     }
 }
